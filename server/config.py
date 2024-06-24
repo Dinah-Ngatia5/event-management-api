7c96cf0f68a5a8ff328248b84c530e4732a797c6
Dinah-Ngatia5 <dinah.ngatia@student.moringaschool.com>
2024-06-24 05:57:52 +0300

    Now using postgres as the database engine

--- server/config.py
+++ server/config.py
@@ -1,11 +1,10 @@
 from dotenv import load_dotenv
 import os
 
-load_dotenv()  
+load_dotenv()  # Load environment variables from a .env file 
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'fallback_secret_key'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///events.db'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://dinah_5:123456@localhost:5432/events_db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    
 
